67d80591245d812beb2a5e3889df2653e1dbcaa3
Azat <az.bilalov@gmail.com>
2024-12-03 00:01:54 +0300

    fix: update message buffer

--- bot/main.go
+++ bot/main.go
@@ -8,6 +8,7 @@ import (
 	"event-automation/config"
 	"fmt"
 	"log"
+	"time"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -30,31 +31,24 @@ func main() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	// TODO: держать вместе userMessages и lastUpdateID
-	lastUpdateID := make(map[int64]int)
-
 	updates := bot.GetUpdatesChan(u)
 
+	// Буфер для сообщений и флаги обработки
+	isProcessing := make(map[int64]bool) // Флаг обработки сообщений для пользователя
+
 	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
 		userID := update.Message.From.ID
 		language := update.Message.From.LanguageCode
 		chatID := update.Message.Chat.ID
 		userState := session.GetState(userID, language)
 
-		if len(handlers.UserMessages[userID]) > 0 && update.UpdateID != lastUpdateID[userID] {
-			fmt.Printf("Processing messages for user %d: %v\n", userID, handlers.UserMessages[userID])
-			sender.SendLocalizedMessage(userID, userState.Language, "processing")
-			// TODO: не передавать экземпляр сообщения, а только нужные свойства
-			err := handlers.CreateEvent(sender, store, update.Message)
-			if err != nil {
-				log.Printf("Error: %v", err)
-			}
-		}
-
 		switch userState.State {
 		case "start":
 			if store.IsExist(chatID) {
-				// TODO: В будущем в этом блоке будем добавлять обработку старта с параметрами
 				sender.SendLocalizedMessage(chatID, userState.Language, "waiting")
 				session.SetState(userID, "awaiting_messages")
 			} else {
@@ -62,33 +56,54 @@ func main() {
 				sender.SendLocalizedMessage(chatID, userState.Language, "register required")
 				session.SetState(userID, "initial")
 			}
+
 		case "initial":
 			registered := handlers.Register(sender, store, update.Message)
-			if !registered {
-				continue
+			if registered {
+				session.SetState(userID, "awaiting_messages")
 			}
-			session.SetState(userID, "awaiting_messages")
 
 		case "awaiting_messages":
 			if update.Message.ForwardFrom != nil || update.Message.ForwardSenderName != "" {
 				handlers.CollectMessage(sender, store, update.Message)
+				if !isProcessing[userID] {
+					isProcessing[userID] = true
+
+					go func(message *tgbotapi.Message) {
+						time.Sleep(2 * time.Second)
+
+						// Обрабатываем накопленные сообщения
+						if len(handlers.UserMessages[userID]) > 0 {
+							fmt.Printf("Processing messages for user %d: %v\n", userID, handlers.UserMessages[userID])
+							sender.SendLocalizedMessage(userID, userState.Language, "processing")
+							handlers.CreateEvent(sender, store, message)
+							if err != nil {
+								log.Printf("Error processing messages for user %d: %v", userID, err)
+							}
+							handlers.UserMessages[userID] = nil
+						}
+						isProcessing[userID] = false
+					}(update.Message)
+				}
 			} else {
 				sender.SendLocalizedMessage(chatID, userState.Language, "waiting")
 			}
+
 		case "change_email":
 			if update.Message.Command() == "yes" {
 				sender.SendLocalizedMessage(chatID, userState.Language, "waiting email")
 				session.SetState(userID, "awaiting_new_email")
-				continue
+			} else {
+				sender.SendLocalizedMessage(chatID, userState.Language, "cancel email change")
+				session.SetState(userID, "awaiting_messages")
 			}
-			sender.SendLocalizedMessage(chatID, userState.Language, "cancel email change")
-			session.SetState(userID, "awaiting_messages")
+
 		case "awaiting_new_email":
 			changeSessionState := handlers.ChangeEmail(sender, store, update.Message)
 			if changeSessionState {
 				session.SetState(userID, "awaiting_messages")
-				continue
 			}
+
 		default:
 			sender.SendLocalizedMessage(chatID, userState.Language, "error")
 		}
